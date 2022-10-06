6a9615454497846245944363aeb3ba20a9f0685b
stecrotti <ruggentipini@gmail.com>
2022-10-06 10:07:27 +0200

    mpem2 with sweeps and simple tests

---
+++ test/mpem.jl
@@ -0,0 +1,25 @@
+include("../mpem.jl")
+
+# tensors = [rand(1,3,2,2), rand(3,4,2,2), rand(4,10,2,2), rand(10,1,2,2)]
+tensors = [rand(1,3,2,2), rand(3,1,2,2)]
+C = MPEM2(tensors)
+T = getT(C)
+
+x = [rand(1:2,2) for t in 1:T+1]
+@show evaluate(C, x)
+@show norm(C)
+
+sweep_RtoL!(C; ε=0.0)
+@show evaluate(C, x)
+@show norm(C)
+# @show bond_dims(C)
+
+sweep_LtoR!(C, ε=0.0)
+# @show bond_dims(C)
+@show evaluate(C, x)
+@show norm(C)
+
+sweep_RtoL!(C; ε=0.0)
+@show evaluate(C, x)
+@show norm(C)
+
