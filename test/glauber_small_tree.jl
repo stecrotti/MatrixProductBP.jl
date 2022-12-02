4560bbb86c195347fa0494916ac2bd12f78c7afe
stecrotti <ruggentipini@gmail.com>
2022-12-02 11:33:07 +0100

    convergence based on messages instead of beliefs

--- test/glauber_small_tree.jl
+++ test/glauber_small_tree.jl
@@ -24,7 +24,7 @@ rng = MersenneTwister(111)
 X = draw_node_observations!(bp, N; rng)
 
 svd_trunc = TruncThresh(0.0)
-cb = CB_BP(bp; svd_trunc, showprogress=false)
+cb = CB_BP(bp; showprogress=false)
 iterate!(bp; maxiter=20, svd_trunc, cb)
 
 b_bp = beliefs(bp; svd_trunc)
