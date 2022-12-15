b8e8cb719486af8c8c845c582bbf76bc1f3e0a65
Alfredo Braunstein <alfredo.braunstein@polito.it>
2022-12-15 17:17:28 +0100

    unify mpems, first try

--- src/MPEMs/MPEMs.jl
+++ src/MPEMs/MPEMs.jl
@@ -2,8 +2,8 @@ module MPEMs
 
 using Reexport
 @reexport import Base:
-    -, eltype, getindex, iterate, firstindex, lastindex, setindex!, length,
-    isapprox
+    -, eltype, getindex, iterate, firstindex, lastindex, setindex!, eachindex, 
+    length, isapprox
 import Lazy: @forward
 import TensorCast: @cast, @reduce, TensorCast
 import LinearAlgebra: svd, norm, normalize!
@@ -18,36 +18,19 @@ export
     accumulate_L, accumulate_R, accumulate_M, pair_marginal, firstvar_marginal,
     pair_marginal_tu, firstvar_marginal_tu, marginals_tu,
     normalization, normalize!,
-    MPEM3
+    MPEM3, MatrixProductTrain
 
 # Matrix Product Edge Message
-abstract type MPEM; end
+
+abstract type MPEM end
+
 
 include("svd_trunc.jl")
+include("mptrain.jl")
 include("mpem1.jl")
 include("mpem2.jl")
 include("mpem3.jl")
 
 
-# keep size of matrix elements under control by dividing by the max
-# return the log of the product of the individual normalizations 
-function normalize_eachmatrix!(A::MPEM)
-    c = 0.0
-    for m in A
-        mm = maximum(abs, m)
-        if !any(isnan, mm) && !any(isinf, mm)
-            m ./= mm
-            c += log(mm)
-        end
-    end
-    c
-end
-
--(A::T, B::T) where {T<:MPEM2} = MPEM2([AA .- BB for (AA,BB) in zip(A.tensors,B.tensors)])
--(A::T, B::T) where {T<:MPEM3} = MPEM3([AA .- BB for (AA,BB) in zip(A.tensors,B.tensors)])
-
-function isapprox(A::T, B::T; kw...) where {T<:MPEM}
-    isapprox(A.tensors, B.tensors; kw...)
-end 
 
 end # end module
