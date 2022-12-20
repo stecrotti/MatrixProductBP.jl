0d987e4cf59caa194d3834caff5e434505d922c7
Alfredo Braunstein <alfredo.braunstein@polito.it>
2022-12-20 17:22:42 +0100

    aesthetics

--- src/bp_core.jl
+++ src/bp_core.jl
@@ -23,6 +23,7 @@ function f_bp(A::Vector{MPEM2{F}}, wᵢ::Vector{U},
     @assert length(wᵢ) == T + 1
     @assert length(ϕᵢ) == T + 1
     q = length(ϕᵢ[1])
+    qj = size(ψₙᵢ[j_index][1],1)
     @assert all(length(ϕᵢᵗ) == q for ϕᵢᵗ in ϕᵢ)
     @assert j_index in eachindex(A)
     z = length(A)      # z = |∂i|
@@ -36,7 +37,7 @@ function f_bp(A::Vector{MPEM2{F}}, wᵢ::Vector{U},
         # select incoming A's but not the j-th one
         Aᵗ = kron2([A[k][t] for k in eachindex(A)[Not(j_index)]]...)
         nrows, ncols = size(Aᵗ, 1), size(Aᵗ, 2)
-        Bᵗ = zeros(nrows, ncols, q, size(ψₙᵢ[j_index][1],1), q)
+        Bᵗ = zeros(nrows, ncols, q, qj, q)
 
         for xᵢᵗ in 1:q
             for xᵢᵗ⁺¹ in 1:q
