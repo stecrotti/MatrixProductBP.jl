cfa387a7e6f45c37d939cc0f0bfa22f5a2009dcb
Alfredo Braunstein <alfredo.braunstein@polito.it>
2022-12-21 17:44:12 +0100

    Fix autocovariances in infinite graph  notebook

--- src/MatrixProductBP.jl
+++ src/MatrixProductBP.jl
@@ -19,7 +19,7 @@ import LogExpFunctions: logistic, logsumexp
 export
     BPFactor, nstates, MPBP, mpbp, reset_messages!, onebpiter!, CB_BP, iterate!, 
     pair_beliefs, pair_beliefs_tu, beliefs_tu, autocorrelations,
-    autocovariances, beliefs, bethe_free_energy, 
+    autocovariances, means, beliefs, bethe_free_energy, 
     logprob, expectation, pair_observations_directed, 
     pair_observations_nondirected, pair_obs_undirected_to_directed,
     exact_prob, exact_marginals, site_marginals, exact_autocorrelations,
@@ -32,11 +32,11 @@ include("utils.jl")
 include("MPEMs/MPEMs.jl")
 using .MPEMs, Reexport
 @reexport import .MPEMs: SVDTrunc, TruncBond, TruncThresh,
-    MPEM, normalize_eachmatrix!, -, isapprox, evaluate, getT, bond_dims,
-    MPEM2, mpem2, rand_mpem2, sweep_RtoL!, sweep_LtoR!,
+    normalize_eachmatrix!, -, isapprox, evaluate, getT, bond_dims,
+    MPEM, MPEM2, MPEM3, MatrixProductTrain, mpem2, rand_mpem2, sweep_RtoL!, sweep_LtoR!,
     accumulate_L, accumulate_R, accumulate_M, pair_marginal, firstvar_marginal,
     pair_marginal_tu, firstvar_marginal_tu, marginals, marginals_tu, mpem1,
-    normalization, normalize!, MPEM3, MatrixProductTrain, nstates, marginalize
+    normalization, normalize!,  nstates, marginalize
 
 include("bp_core.jl")
 include("mpbp.jl")
