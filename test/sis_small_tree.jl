bd19af338978334a9195b475ded8df64345d5696
Alfredo Braunstein <alfredo.braunstein@polito.it>
2022-12-21 11:10:31 +0100

    Add FakeSIS tests

--- test/sis_small_tree.jl
+++ test/sis_small_tree.jl
@@ -1,3 +1,8 @@
+using MatrixProductBP, MatrixProductBP.Models, IndexedGraphs, Test, Random
+import MatrixProductBP: nstates
+import MatrixProductBP.Models: prob_xy, prob_yy, prob_y
+
+
 
 T = 3
 
@@ -13,6 +18,11 @@ sis = SIS(g, λ, ρ, T; γ)
 bp = mpbp(sis)
 rng = MersenneTwister(111)
 X, _ = onesample(bp; rng)
+
+@testset "logprob" begin
+    @test logprob(bp, X) ≈ -8.50477067141768 
+end
+
 draw_node_observations!(bp.ϕ, X, N, last_time=true; rng)
 
 svd_trunc = TruncThresh(0.0)
@@ -42,6 +52,35 @@ c_exact = exact_autocovariances(f, bp; r = r_exact)
     @test r_bp ≈ r_exact
     @test c_bp ≈ c_exact
 end
+
+struct FakeSIS <: RecursiveBPFactor
+    w::SISFactor
+end
+
+prob_xy(w::FakeSIS, x...) = prob_xy(w.w, x...)
+prob_yy(w::FakeSIS, x...) = prob_yy(w.w, x...)
+prob_y(w::FakeSIS, x...) = prob_y(w.w, x...)
+
+nstates(::Type{<:FakeSIS}, l::Int) = nstates(SISFactor, l)
+
+
+@testset "FakeSIS - RecursiveBPFactor generic methods" begin
+    rng2 = MersenneTwister(111)
+    bpfake = MPBP(bp.g, [FakeSIS.(w) for w in bp.w], bp.ϕ, bp.ψ, deepcopy(bp.μ), deepcopy(bp.b))
+
+    for i=1:20
+        X, _ = onesample(bp; rng)
+        @test logprob(bp, X) ≈ logprob(bpfake, X)
+    end
+
+    iterate!(bpfake, maxiter=10; svd_trunc, showprogress=false)
+
+    @test beliefs(bpfake) ≈ beliefs(bp)
+
+end
+
+
+
 
 # observe everything and check that the free energy corresponds to the posterior of sample `X`
 sis = SIS(g, λ, ρ, T; γ)
