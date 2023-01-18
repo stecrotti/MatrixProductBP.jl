d2de29032d39f1b28d4b06c0bf01740c2e6ff4af
stecrotti <ruggentipini@gmail.com>
2023-01-18 08:09:27 +0100

    define pmJ glauber factor

--- src/Models/glauber/glauber_bp.jl
+++ src/Models/glauber/glauber_bp.jl
@@ -3,18 +3,6 @@ struct GenericGlauberFactor{T<:Real}  <: BPFactor
     βh :: T
 end
 
-struct HomogeneousGlauberFactor{T<:Real} <: RecursiveBPFactor 
-    βJ :: T     
-    βh :: T
-end
-
-# the sum of `l` spins can assume `l+1` values
-nstates(::Type{<:HomogeneousGlauberFactor}, l::Integer) = l + 1
-
-function HomogeneousGlauberFactor(J::T, h::T, β::T) where {T<:Real}
-    HomogeneousGlauberFactor(J*β, h*β)
-end
-
 function GenericGlauberFactor(J::Vector{T}, h::T, β::T) where {T<:Real}
     GenericGlauberFactor(J.*β, h*β)
 end
@@ -31,17 +19,58 @@ function (fᵢ::GenericGlauberFactor)(xᵢᵗ⁺¹::Integer,
     exp( -E ) / (2cosh(E))
 end
 
-function (fᵢ::HomogeneousGlauberFactor)(xᵢᵗ⁺¹::Integer, 
+struct HomogeneousGlauberFactor{T<:Real} <: RecursiveBPFactor 
+    βJ :: T     
+    βh :: T
+end
+
+function HomogeneousGlauberFactor(J::T, h::T, β::T) where {T<:Real}
+    HomogeneousGlauberFactor(J*β, h*β)
+end
+
+# the sum of `l` spins can assume `l+1` values
+nstates(::Type{<:HomogeneousGlauberFactor}, l::Integer) = l + 1
+
+
+# ignore neighbor because it doesn't exist
+function prob_y(wᵢ::HomogeneousGlauberFactor, xᵢᵗ⁺¹, xᵢᵗ, zᵗ, d)
+    @unpack βJ, βh = wᵢ
+    yᵗ = 2 * zᵗ - 2 - d
+    h = βJ * yᵗ + βh
+    p = exp(potts2spin(xᵢᵗ⁺¹) * h) / (2*cosh(h))
+    @assert 0 ≤ p ≤ 1
+    p
+end
+
+prob_xy(wᵢ::HomogeneousGlauberFactor, yₖ, xₖ, xᵢ) = (yₖ != xₖ)
+prob_yy(wᵢ::HomogeneousGlauberFactor, y, y1, y2, xᵢ) = (y == y1 + y2 - 1)
+
+function (wᵢ::HomogeneousGlauberFactor)(xᵢᵗ⁺¹::Integer, 
         xₙᵢᵗ::AbstractVector{<:Integer}, 
         xᵢᵗ::Integer)
     @assert xᵢᵗ⁺¹ ∈ 1:2
     @assert all(x ∈ 1:2 for x in xₙᵢᵗ)
 
-    hⱼᵢ = sum( Jᵢⱼ * potts2spin(xⱼᵗ) for (xⱼᵗ,Jᵢⱼ) in zip(xₙᵢᵗ, fᵢ.βJ))
-    hⱼᵢ = fᵢ.βJ * sum(potts2spin, xₙᵢᵗ)
-    E = - potts2spin(xᵢᵗ⁺¹) * (hⱼᵢ + fᵢ.βh)
+    hⱼᵢ = sum( Jᵢⱼ * potts2spin(xⱼᵗ) for (xⱼᵗ,Jᵢⱼ) in zip(xₙᵢᵗ, wᵢ.βJ))
+    hⱼᵢ = wᵢ.βJ * sum(potts2spin, xₙᵢᵗ)
+    E = - potts2spin(xᵢᵗ⁺¹) * (hⱼᵢ + wᵢ.βh)
     exp( -E ) / (2cosh(E))
 end
+
+# Ising model with ±J interactions
+struct PMJGlauberFactor{T<:Real} <: RecursiveBPFactor
+    signs :: BitVector
+    βJ    :: T     
+    βh    :: T
+end
+
+nstates(::Type{<:PMJGlauberFactor}, l::Integer) = l + 1
+
+bool2sign(x::Bool) = 1 - 2x
+sign2bool(s::Integer) = (@assert abs(s) == 1; (1 - s) / 2)
+
+
+
 
 function mpbp(gl::Glauber{T,N,F}; kw...) where {T,N,F<:AbstractFloat}
     g = IndexedBiDiGraph(gl.ising.g.A)
@@ -68,16 +97,3 @@ function glauber_factors(ising::Ising, T::Integer)
         fill(wᵢᵗ, T + 1)
     end
 end
-
-# ignore neighbor because it doesn't exist
-function prob_y(wᵢ::HomogeneousGlauberFactor, xᵢᵗ⁺¹, xᵢᵗ, zᵗ, d)
-    @unpack βJ, βh = wᵢ
-    yᵗ = 2 * zᵗ - 2 - d
-    h = βJ * yᵗ + βh
-    p = exp(potts2spin(xᵢᵗ⁺¹) * h) / (2*cosh(h))
-    @assert 0 ≤ p ≤ 1
-    p
-end
-
-prob_xy(wᵢ::HomogeneousGlauberFactor, yₖ, xₖ, xᵢ) = (yₖ != xₖ)
-prob_yy(wᵢ::HomogeneousGlauberFactor, y, y1, y2, xᵢ) = (y == y1 + y2 - 1)
