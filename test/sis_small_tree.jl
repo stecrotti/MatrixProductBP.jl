9c85e99be61212231db861f58ff097094ebf6a8c
Alfredo Braunstein <alfredo.braunstein@polito.it>
2023-01-18 10:30:21 +0100

    move RecursiveTraceFactor to tests

--- test/sis_small_tree.jl
+++ test/sis_small_tree.jl
@@ -79,6 +79,24 @@ nstates(::Type{<:FakeSIS}, l::Int) = nstates(SISFactor, l)
 
 end
 
+struct RecursiveTraceFactor{F<:BPFactor,N} <: RecursiveBPFactor
+    w :: F
+end
+
+RecursiveTraceFactor(f,N) = RecursiveTraceFactor{typeof(f),N}(f)
+
+nstates(::Type{RecursiveTraceFactor{F,N}}, d::Integer) where {F,N} = N^d
+
+function prob_y(wᵢ::U, xᵢᵗ⁺¹, xᵢᵗ, yₙᵢᵗ, dᵢ) where {U <: RecursiveTraceFactor{F,N}} where {F,N}
+    wᵢ.w(xᵢᵗ⁺¹, reverse(digits(yₙᵢᵗ-1; base=N, pad=dᵢ)) .+ 1, xᵢᵗ)
+end
+
+prob_xy(wᵢ::RecursiveTraceFactor, yₖ, xₖ, xᵢ, k) = (yₖ == xₖ)
+
+prob_yy(wᵢ::U, y, y1, y2, xᵢ, d1, d2) where {U<:RecursiveTraceFactor} = (y - 1 == (y1 - 1)  + (y2 - 1) * d1)
+
+
+
 @testset "RecursiveTraceFactor" begin
     rng2 = MersenneTwister(111)
     bpfake = MPBP(bp.g, [RecursiveTraceFactor.(w,2) for w in bp.w], bp.ϕ, bp.ψ, 
