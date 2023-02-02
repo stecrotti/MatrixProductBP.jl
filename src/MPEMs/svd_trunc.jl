087688f651c45c8b7f5d5e2a8cb7320c4be1d76c
stecrotti <sky_96@live.it>
2023-02-02 09:26:13 +0100

    new svd truncator useful for experiments

--- src/MPEMs/svd_trunc.jl
+++ src/MPEMs/svd_trunc.jl
@@ -40,3 +40,24 @@ end
 function show(io::IO, svd_trunc::TruncBond)
     println(io, "SVD truncation to bond size m'=", svd_trunc.mprime)
 end
+
+# truncates matrices to size `mprime`, stores the maximum error
+struct TruncBondMax <: SVDTrunc
+    mprime :: Int
+    maxerr :: Vector{Float64}
+    TruncBondMax(mprime::Int) = new(mprime, [0.0])
+end
+
+function (svd_trunc::TruncBondMax)(M::AbstractMatrix)
+    U, λ, V = svd(M)
+    mprime = min(length(λ), svd_trunc.mprime)
+    _debug_svd(M, U, λ, V, mprime)
+    err = sum(abs2, @view λ[mprime+1:end]) / sum(abs2, λ) |> sqrt
+    svd_trunc.maxerr[1] = max(svd_trunc.maxerr[1], err) 
+    U[:,1:mprime], λ[1:mprime], V[:,1:mprime]
+end
+
+function Base.show(io::IO, svd_trunc::TruncBondMax)
+    println(io, "SVD truncation to bond size m'=", svd_trunc.mprime,
+        ". Max error ", only(svd_trunc.maxerr))
+end
