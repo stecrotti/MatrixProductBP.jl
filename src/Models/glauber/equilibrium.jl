2c022309cdd5c21cb131305646cf0dae53c0fc3e
stecrotti <sky_96@live.it>
2023-02-13 17:02:52 +0100

    equilibrium stuff, with tests

--- src/Models/glauber/equilibrium.jl
+++ src/Models/glauber/equilibrium.jl
@@ -28,25 +28,38 @@ function equilibrium_magnetization(g::RandomRegular, J::Real; β::Real=1.0, h::R
     end
 
     ustar = iterate_fixedpoint(f, init; maxiter, tol)
-    return tanh(β*(k*ustar-h)/(k-1))
+    return abs( tanh(β*(k*ustar-h)/(k-1)) )
 end
 
 
-function equilibrium_magnetization(g::ErdosRenyi, J::Real; β::Real=1.0, h::Real=0.0,
-        popsize=10^3, maxiter=10^3, tol=1/sqrt(popsize), nsamples=10^3)
-    c = g.c
+function equilibrium_magnetization(g::ErdosRenyi; kw...)
+    equilibrium_magnetization(Poisson(g.c), Poisson(g.c); kw...)
+end
+
+function equilibrium_magnetization(g::RandomRegular; kw...)
+    equilibrium_magnetization(Dirac(g.k-1), Dirac(g.k); kw...)
+end
+
+# pkm1 is the residual probability: prob that a random edge is incident on a var of degree k
+# pk is the degree profile
+function equilibrium_magnetization(pkm1::Distribution, pk::Distribution; 
+        pJ::Distribution=Dirac(1.0), β::Real=1.0,
+        ph::Distribution=Dirac(0.0), popsize=10^3, maxiter=10^3, tol=0.1/sqrt(popsize), 
+        nsamples=10^3, rng=Random.GLOBAL_RNG)
     m = zeros(nsamples)
+    us = zeros(nsamples)
     
     P = randn(popsize)
     Pnew = copy(P)
 
-    f(us, J, β, h) = 1/β*sum(atanh(tanh(β*u)*tanh(β*J)) for u in us; init=0.0) + h
+    f(us, Js, β, h) = 1/β*sum(atanh(tanh(β*u)*tanh(β*J)) for (u,J) in zip(us,Js); init=0.0) + h
 
-    function iterate_ER!(P, f, c, J, β, h)
+    function iterate_bp!(P, f, pkm1, pJ, β, ph)
         for idx_out in eachindex(P)
-            km1 = rand(Poisson(c))
+            km1 = rand(pkm1)
             idx_in = rand(eachindex(P), km1)
-            P[idx_out] = f((@view P[idx_in]), J, β, h)
+            Js = rand(rng, pJ, km1); h = rand(ph)
+            P[idx_out] = f((@view P[idx_in]), Js, β, h)
         end
         nothing
     end
@@ -54,7 +67,7 @@ function equilibrium_magnetization(g::ErdosRenyi, J::Real; β::Real=1.0, h::Real
     iters = 0; err = Inf
     prog = Progress(maxiter, desc="Running population dynamics")
     for _ in 1:maxiter
-        iterate_ER!(Pnew, f, c, J, β, h)
+        iterate_bp!(Pnew, f, pkm1, pJ, β, ph)
         err = abs(mean(P) - mean(Pnew)) 
         err < tol && break
         P, Pnew = Pnew, P
@@ -66,10 +79,12 @@ function equilibrium_magnetization(g::ErdosRenyi, J::Real; β::Real=1.0, h::Real
     iters == maxiter && @warn "Population dynamics did not converge. Error $err"
 
     for s in 1:nsamples
-        k = rand(Poisson(c))
+        k = rand(pk)
         idx_in = rand(eachindex(P), k)
-        u = f(P[idx_in], J, β, h)
+        Js = rand(rng, pJ, k); h = rand(ph)
+        u = f(P[idx_in], Js, β, h)
         m[s] = tanh(β*u)
+        us[s] = u
     end
 
     m_avg = mean(abs.(m))
