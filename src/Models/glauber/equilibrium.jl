7a0ca5b6dcb52a04736795306a9460e69892bc14
stecrotti <sky_96@live.it>
2023-02-14 16:59:55 +0100

    callback for equilibrium computations

--- src/Models/glauber/equilibrium.jl
+++ src/Models/glauber/equilibrium.jl
@@ -31,6 +31,35 @@ function equilibrium_magnetization(g::RandomRegular, J::Real; β::Real=1.0, h::R
     return abs( tanh(β*(k*ustar-h)/(k-1)) )
 end
 
+# A callback to print info and save stuff during the iterations 
+struct CB_Pop{TP<:ProgressUnknown, F}
+    prog :: TP
+    f    :: F
+    m    :: Vector{Float64} 
+    Δs   :: Vector{Float64}
+
+    function CB_Pop(; showprogress::Bool=true, f::F=mean, info="") where F
+        dt = showprogress ? 0.1 : Inf
+        isempty(info) || (info *= "\n")
+        prog = ProgressUnknown(desc=info*"Running PopDyn: iter", dt=dt)
+        TP = typeof(prog)
+
+        m = [Inf]
+        Δs = zeros(0)
+        new{TP,F}(prog, f, m, Δs)
+    end
+end
+
+function (cb::CB_Pop)(P::Vector{Float64}, it::Integer, maxiter::Integer, tol)
+    mnew = cb.f(P)
+    Δ = abs(mnew - cb.m[end]) 
+    push!(cb.Δs, Δ)
+    push!(cb.m, mnew)
+    next!(cb.prog, showvalues=[(:it, "$it/$maxiter"), 
+        (:ε,"$(round(Δ, digits=ceil(Int, log10(1/tol))))/$tol")])
+    return Δ
+end
+
 
 function equilibrium_magnetization(g::ErdosRenyi; kw...)
     equilibrium_magnetization(Poisson(g.c), Poisson(g.c); kw...)
@@ -45,50 +74,46 @@ end
 function equilibrium_magnetization(pkm1::Distribution, pk::Distribution; 
         pJ::Distribution=Dirac(1.0), β::Real=1.0,
         ph::Distribution=Dirac(0.0), popsize=10^3, maxiter=10^3, tol=0.1/sqrt(popsize), 
-        nsamples=10^3, rng=Random.GLOBAL_RNG)
-    m = zeros(nsamples)
-    us = zeros(nsamples)
+        nsamples=10^3, rng=GLOBAL_RNG,
+        P = randn(rng, popsize), cb=CB_Pop())
     
-    P = randn(popsize)
+    m = zeros(nsamples)
     Pnew = copy(P)
 
     f(us, Js, β, h) = 1/β*sum(atanh(tanh(β*u)*tanh(β*J)) for (u,J) in zip(us,Js); init=0.0) + h
 
-    function iterate_bp!(P, f, pkm1, pJ, β, ph)
-        for idx_out in eachindex(P)
-            km1 = rand(pkm1)
-            idx_in = rand(eachindex(P), km1)
-            Js = rand(rng, pJ, km1); h = rand(ph)
+    function iterate_bp!(P, f, pkm1, pJ, β, ph, indices)
+        for idx_out in indices
+            km1 = rand(rng, pkm1)
+            idx_in = rand(rng, eachindex(P), km1)
+            Js = rand(rng, pJ, km1); h = rand(rng, ph)
             P[idx_out] = f((@view P[idx_in]), Js, β, h)
         end
         nothing
     end
 
-    iters = 0; err = Inf
-    prog = Progress(maxiter, desc="Running population dynamics")
-    for _ in 1:maxiter
-        iterate_bp!(Pnew, f, pkm1, pJ, β, ph)
-        err = abs(mean(P) - mean(Pnew)) 
-        err < tol && break
+    Δ = Inf
+    indices = collect(eachindex(P))
+    for it in 1:maxiter
+        iterate_bp!(Pnew, f, pkm1, pJ, β, ph, indices)
+        Δ = cb(Pnew, it, maxiter, tol)
+        Δ < tol && break
         P, Pnew = Pnew, P
-        iters += 1
-        next!(prog, showvalues=[(:it, "$iters/$maxiter"), 
-            (:ε,"$(round(err, digits=ceil(Int, log10(1/tol))))/$tol")])
+        shuffle!(rng, indices)
     end
 
-    iters == maxiter && @warn "Population dynamics did not converge. Error $err"
+    Δ < tol || @warn "Population dynamics did not converge. Error $Δ"
 
     for s in 1:nsamples
-        k = rand(pk)
-        idx_in = rand(eachindex(P), k)
+        k = rand(rng, pk)
+        idx_in = rand(rng, eachindex(P), k)
         Js = rand(rng, pJ, k); h = rand(ph)
         u = f(P[idx_in], Js, β, h)
         m[s] = tanh(β*u)
-        us[s] = u
     end
 
-    m_avg = mean(abs.(m))
-    m_std = std(abs.(m)) / sqrt(length(m))
+    m_avg = abs(mean(m))
+    m_std = std(m) / sqrt(length(m))
 
     return m_avg ± m_std
 end
