5b259da8d84d559036d3bfed2295f88613bfea54
stecrotti <sky_96@live.it>
2023-02-23 12:16:07 +0100

    include ALL tests, order includes alphabetically

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,14 +2,16 @@ using Test, MatrixProductBP
 using MatrixProductBP.Models
 using Graphs, IndexedGraphs, Random, Distributions
 
+include("equilibrium.jl")
+include("glauber_infinite_graph.jl")
+include("glauber_pmJ_small_tree.jl")
+include("glauber_small_tree.jl")
 include("mpem.jl")
 include("normalizations.jl")
-include("glauber_small_tree.jl")
 include("pair_observations.jl")
-include("glauber_pmJ_small_tree.jl")
-include("sis_small_tree.jl")
+include("sampling.jl")
 include("sirs_small_tree.jl")
 include("sis_infinite_graph.jl")
-include("sampling.jl")
+include("sis_small_tree.jl")
 
 nothing
