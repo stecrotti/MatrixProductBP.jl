e6cdd8850e4981cb6afeabbb84bea383a9fee19d
stecrotti <sky_96@live.it>
2023-03-28 16:56:02 +0200

    test DampedFacor

--- test/glauber_small_tree.jl
+++ test/glauber_small_tree.jl
@@ -57,7 +57,7 @@ c_exact = exact_autocovariances(f, bp; r = r_exact)
 end
 
 # observe everything and check that the free energy corresponds to the posterior of sample `X`
-reset!(bp)
+reset!(bp; observations=true)
 draw_node_observations!(bp.ϕ, X, N*(T+1), last_time=false)
 reset_messages!(bp)
 cb = CB_BP(bp; showprogress=false)
@@ -68,4 +68,40 @@ logp = logprob(bp, X)
 
 @testset "Glauber small tree - observe everything" begin
     @test logl_bp ≈ logp
+end
+
+# test DampedFactor
+reset!(bp; observations=true)
+X, observed = draw_node_observations!(bp, N; rng)
+p = 0.2
+w = [[DampedFactor(www, p) for www in ww] for ww in bp.w]
+bp2 = mpbp(bp.g, w, fill(2,N), T; ϕ = bp.ϕ)
+
+svd_trunc = TruncBondThresh(10)
+cb = CB_BP(bp2; showprogress=false, info="Glauber")
+iterate!(bp2; maxiter=20, svd_trunc, cb)
+
+b_bp = beliefs(bp2)
+p_bp = [[bbb[2] for bbb in bb] for bb in b_bp]
+
+p_exact, Z_exact = exact_prob(bp2)
+b_exact = exact_marginals(bp2; p_exact)
+p_ex = [[bbb[2] for bbb in bb] for bb in b_exact]
+
+f_bethe = bethe_free_energy(bp2)
+Z_bp = exp(-f_bethe)
+
+f(x,i) = 2x-3
+
+r_bp = autocorrelations(f, bp2)
+r_exact = exact_autocorrelations(f, bp2; p_exact)
+
+c_bp = autocovariances(f, bp2)
+c_exact = exact_autocovariances(f, bp2; r = r_exact)
+
+@testset "Glauber small tree - DampedFactor" begin
+    @test Z_exact ≈ Z_bp
+    @test p_ex ≈ p_bp
+    @test r_bp ≈ r_exact
+    @test c_bp ≈ c_exact
 end
