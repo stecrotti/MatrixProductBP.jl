9e9653a90edecd2b2b7692396e4196ef8a518007
stecrotti <sky_96@live.it>
2023-04-27 17:18:16 +0200

    plots with discrete CME

--- notebooks/meanfield.jl
+++ notebooks/meanfield.jl
@@ -27,6 +27,23 @@ function dmp_disc(T, Δt, g, λ_rate, ρ_rate, γ)
         end
         for (i, j, id) in edges(g)
            ϕ[id][t] = (1-ρ)*ϕ[id][t-1] + 
+                (1-r[i][t-1])*(1-prod(1-λ*ϕ[idx(e)][t-1] for e in inedges(g, i) if src(e)!=j; init=1.0))
+        end
+        next!(prog)
+    end
+    r, ϕ
+end
+
+# cavity master equation
+function cme_disc(T, Δt, g, λ_rate, ρ_rate, γ)
+    Tdisc, λ, ρ, prog, r, ϕ = initialize(T, Δt, g, λ_rate, ρ_rate, γ)
+    for t in 2:Tdisc+1
+        for i in vertices(g)
+           r[i][t] = (1-ρ)*r[i][t-1] + 
+                (1-r[i][t-1])*(1-prod(1-λ*ϕ[idx(e)][t-1] for e in inedges(g, i); init=1.0))
+        end
+        for (i, j, id) in edges(g)
+           ϕ[id][t] = (1-ρ)*ϕ[id][t-1] + 
                 (1-ϕ[id][t-1])*(1-prod(1-λ*ϕ[idx(e)][t-1] for e in inedges(g, i) if src(e)!=j; init=1.0))
         end
         next!(prog)
