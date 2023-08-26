57ec13c5687196246d457d17a59373463486a26d
stecrotti <sky_96@live.it>
2023-08-26 11:01:26 +0200

    support generic factors with larger degree

--- src/utils.jl
+++ src/utils.jl
@@ -17,6 +17,22 @@ function kron2(A₁::Array{F,4}, A₂::Array{F,4}, A₃::Array{F,4}) where F
     @cast _[(m₁, m₂, m₃), (n₁, n₂, n₃), xᵢ, x₁, x₂, x₃] := 
         A₁[m₁, n₁, x₁, xᵢ] * A₂[m₂, n₂, x₂, xᵢ] * A₃[m₃, n₃, x₃, xᵢ]
 end
+function kron2(A₁::Array{F,4}, A₂::Array{F,4}, A₃::Array{F,4}, A₄::Array{F,4}) where F
+    @cast _[(m₁, m₂, m₃, m₄), (n₁, n₂, n₃, n₄), xᵢ, x₁, x₂, x₃, x₄] := 
+        A₁[m₁, n₁, x₁, xᵢ] * A₂[m₂, n₂, x₂, xᵢ] * A₃[m₃, n₃, x₃, xᵢ] * A₄[m₄, n₄, x₄, xᵢ]
+end
+function kron2(A₁::Array{F,4}, A₂::Array{F,4}, A₃::Array{F,4}, A₄::Array{F,4}, 
+        A₅::Array{F,4}) where F
+    @cast _[(m₁, m₂, m₃, m₄, m₅), (n₁, n₂, n₃, n₄, n₅), xᵢ, x₁, x₂, x₃, x₄, x₅] := 
+        A₁[m₁, n₁, x₁, xᵢ] * A₂[m₂, n₂, x₂, xᵢ] * A₃[m₃, n₃, x₃, xᵢ] * A₄[m₄, n₄, x₄, xᵢ] *
+            A₅[m₅, n₅, x₅, xᵢ]
+end
+function kron2(A₁::Array{F,4}, A₂::Array{F,4}, A₃::Array{F,4}, A₄::Array{F,4}, 
+    A₅::Array{F,4}, A₆::Array{F,4}) where F
+@cast _[(m₁, m₂, m₃, m₄, m₅, m₆), (n₁, n₂, n₃, n₄, n₅, n₆), xᵢ, x₁, x₂, x₃, x₄, x₅, x₆] := 
+    A₁[m₁, n₁, x₁, xᵢ] * A₂[m₂, n₂, x₂, xᵢ] * A₃[m₃, n₃, x₃, xᵢ] * A₄[m₄, n₄, x₄, xᵢ] *
+        A₅[m₅, n₅, x₅, xᵢ] * A₆[m₆, n₆, x₆, xᵢ]
+end
 
 # SAMPLING
 # sample an index `i` of `w` with probability prop to `w[i]`
