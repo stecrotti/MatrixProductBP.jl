60837554c58aafad4f079cbb086c3331e22b4325
stecrotti <sky_96@live.it>
2024-04-26 11:54:31 +0200

    tests pass

--- test/glauber_infinite_graph.jl
+++ test/glauber_infinite_graph.jl
@@ -51,16 +51,20 @@ end
     m⁰ = 0.5
 
     β = 1.0
-    J = 1.0
+    JA = 1.0
+    JB = -0.2
     h = 0.0
 
     fill2(x) = fill(x, 2)
 
-    wᵢ = fill(HomogeneousGlauberFactor(J, h, β), T+1)
-    ϕᵢ = [ t == 0 ? [(1+m⁰)/2, (1-m⁰)/2] : ones(2) for t in 0:T]
-    # ϕᵢ[1][2] = [0.4, 0.6]
-    # ϕᵢ[2][end] = [0.95, 0.05]
-    bp = mpbp_infinite_bipartite_graph(k, fill2(wᵢ), (2, 2), fill2(ϕᵢ))
+    w = [
+        fill(HomogeneousGlauberFactor(JA, h, β), T+1),
+        fill(HomogeneousGlauberFactor(JB, h, β), T+1)
+    ]
+    ϕ = [[t == 0 ? [(1+m⁰)/2, (1-m⁰)/2] : ones(2) for t in 0:T] for i in 1:2]
+    ϕ[1][2] = [0.4, 0.6]
+    ϕ[2][end] = [0.95, 0.05]
+    bp = mpbp_infinite_bipartite_graph(k, w, (2, 2), ϕ)
     cb = CB_BP(bp)
 
     iters, cb = iterate!(bp; maxiter=150, svd_trunc=TruncThresh(0.0), cb, tol=1e-15, damp=0.1)
@@ -74,8 +78,17 @@ end
 
     N = sum(k)
     g = IndexedBiDiGraph(complete_bipartite_graph(reverse(k)...))
-    bp_exact = mpbp(g, fill(wᵢ, N), fill(2,N), T)
-    for i in 1:N; bp_exact.ϕ[i] = ϕᵢ; end
+    w_exact = map(vertices(g)) do i
+        fill(HomogeneousGlauberFactor(i ≤ k[2] ? β*JA : β*JB, β*h), T+1)
+    end
+    bp_exact = mpbp(g, w_exact, fill(2,N), T)
+    for i in 1:N
+        if i ≤ k[2]
+            bp_exact.ϕ[i] = ϕ[1]
+        else
+            bp_exact.ϕ[i] = ϕ[2]
+        end
+    end
 
     iterate!(bp_exact; maxiter=150, svd_trunc=TruncThresh(0.0), cb, tol=1e-15)
 
