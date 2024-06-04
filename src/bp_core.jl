ac908e6c77cb919ed8b577839ac329b85c40ae09
stecrotti <sky_96@live.it>
2024-06-04 14:45:50 +0200

    add everything for uniform messages, except truncations

--- src/bp_core.jl
+++ src/bp_core.jl
@@ -2,7 +2,7 @@
 Factor for the factor graph of a model solvable with MPBP.
 
 Any `BPFactor` subtype must implement:
-- A functor that computes the Boltzmann contribution to the joint probability
+- A functor that computes the factor contribution to the joint probability
 
 That's it!
 
